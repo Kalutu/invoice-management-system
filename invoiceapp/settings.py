7f9154f76b84a26d5da57886156921a0d2b7c2b0
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-25 15:08:00 +0300

    Set up database

--- invoiceapp/settings.py
+++ invoiceapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'invoiceapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'invoicemgt',
+        'USER': 'Kalutu',
+        'PASSWORD': os.environ['DB_PASS'],
     }
 }
 
--- manage.py
+++ manage.py
@@ -2,7 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def main():
     """Run administrative tasks."""
