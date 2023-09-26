45ca68fa937565c08a235bb30cb54288591d0867
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-26 14:21:03 +0300

    Styled the form

--- invoice/forms.py
+++ invoice/forms.py
@@ -6,5 +6,5 @@ class InvoiceForm(forms.ModelForm):
 		model = Invoice
 		fields = ['name', 'phone_number', 'invoice_date',
 				'line_one', 'line_one_quantity', 'line_one_unit_price', 'line_one_total_price',
-				'total', 'paid', 'invoice_type'
+				'total', 'invoice_type', 'paid'
 				]
--- invoiceapp/settings.py
+++ invoiceapp/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'invoiceapp',
     'invoice',
-    'bootstrap5'
+    'bootstrap5',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
