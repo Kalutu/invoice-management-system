aff01ab3629a84a7613cb850d455c0b4ac963ae8
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-27 12:16:48 +0300

    Autofill a field in django

--- invoice/static/js/myjs.js
+++ invoice/static/js/myjs.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    console.log("We are all set!");
+  $('#id_line_one_quantity, #id_line_one_unit_price').keyup(function(){
+    var line_one_quantity_text = $('#id_line_one_quantity').val();
+    var line_one_unit_price_text = $('#id_line_one_unit_price').val();
+    var line_one_total = line_one_quantity_text * line_one_unit_price_text;
+
+    var total = line_one_total;
+    
+    $('#id_line_one_total_price').val(line_one_total);
+    $('#id_total').val(total);
+});
 });
